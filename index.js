7765893a53e9757d32580d3d3ac79b46262c2180
Vishv Shah <vishvshahwork@gmail.com>
2023-04-28 22:33:15 -0400

    cors header

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ const app = express();
 app.use(cors({origin: 'http://localhost:3000/'})); 
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 
 // define a route to fetch data based on decade
